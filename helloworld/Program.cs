8af78fb79945ec46e6dfa81b0af01b70a1340096
Emir Gegic <gegeeem@gmail.com>
2024-03-19 10:45:35 +0100

    Trim(), TrimStart, TrimEnd  - methods

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! I'm here!");
+
+string firstFriend = "Maria";
+string secondFriend = "Scott";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine("My friend is "+firstFriend+" and "+secondFriend);
+
+
+//Trim() method
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
