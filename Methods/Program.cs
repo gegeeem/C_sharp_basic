19d590c7660ac665ca29f9e87a5e9a58dcb259c3
Emir Gegic <gegeeem@gmail.com>
2024-03-22 22:39:05 +0100

    int methods

---
+++ Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+    int max = int.MaxValue;
+    Console.WriteLine("Max int value: "+max);
+    int d = int.Min(a,b);
+    Console.WriteLine("int d = int.Min(a,b): "+d);
+    int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+}
