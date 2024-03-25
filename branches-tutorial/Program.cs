12a79b7418afab1c6cdfb3aa352eae1c538cf876
Emir Gegic <gegeeem@gmail.com>
2024-03-26 00:48:25 +0100

    statements, loops

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -3,3 +3,24 @@ int b = 6;
 if (a + b > 10)
     Console.WriteLine("The answer is greater than 10.");
    Console.WriteLine(a > b ? "ok": "no");
+
+   //
+   int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+}
+//
+int counter2 = 0;
+do
+{
+    Console.WriteLine($"Hello World! The counter2 is {counter2}");
+    counter2++;
+} while (counter2 < 10);
+
+//
+for (int index = 0; index < 10; index++)
+{
+    Console.WriteLine($"Hello World! The index is {index}");
+}
