06ea65abb58c00bf241c825916256fc47e079a0b
Emir Gegic <gegeeem@gmail.com>
2024-04-02 18:54:58 +0200

    comm

--- helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66afef6d0c8145bfe347fc02a0aecb70c664cbee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32fc844ed5ab260e2c63526da3d59e064c1542a")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
