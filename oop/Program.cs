bb977f7c072d2f3837daa4d11aef8355851f8343
Emir Gegic <gegeeem@gmail.com>
2024-04-03 18:53:15 +0200

    create obj, by constructor

---
+++ oop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyNamespaces
+{
+    public class Person{
+        public string name;
+        public string surname;
+        private DateOnly dateOfB;
+        public Person(string _name, string _surname, DateOnly _dateOfB){
+            name = _name;
+            surname = _surname;
+            dateOfB = _dateOfB;
+
+        }
+        public string getDateOfB(){
+            return dateOfB.ToString();
+        }
+    }
+public class MyApp{
+    public static void Main(){
+        Person me = new Person("Emir", "Gegic", new DateOnly(1990, 9, 3) );
+        Console.WriteLine($"Name: {me.name}, Surname: {me.surname}, Date of birthday: {me.getDateOfB()}");
+    }
+
+}
+    
+}
